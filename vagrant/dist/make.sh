02c3a2c65bbe9a9f74c2d99d4c7c378adbd1ff02
messfish <messfish@users.noreply.github.com>
2017-04-02 11:10:33 -0700

    Add files via upload

---
+++ vagrant/dist/make.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+set -x
+
+BASE_BOX_ROOT=~/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-trusty64
+
+if [[ ! -d $BASE_BOX_ROOT ]]; then
+    echo "You need to do 'vagrant box add ubuntu/trusty64' first!" >&2
+    exit 1
+fi
+
+BASE_BOX_VERSION=`ls -d1 $BASE_BOX_ROOT/[0-9]* | tail -1`
+
+echo "Detected base box version $BASE_BOX_VERSION"
+
+if [[ ! -f $BASE_BOX_VERSION/virtualbox/box-disk1.vmdk ]]; then
+    echo "Could not find box-disk1.vmdk from base box"
+    exit 1
+fi
+
+if grep "REPLACEME" Vagrantfile; then
+    echo "Please replace the MAC address in dist/Vagrantfile with the actual image MAC" >&2
+    exit 1
+fi
+
+if [[ ! -e box.ovf ]]; then
+    ln -s $BASE_BOX_VERSION/virtualbox/box.ovf box.ovf
+fi
+
+if [[ ! -e box-disk1.vmdk ]]; then
+    ln -s $BASE_BOX_VERSION/virtualbox/box-disk1.vmdk box-disk1.vmdk
+fi
+
+tar czvf spring2017.box -h manifests modules include box-disk1.vmdk box.ovf Vagrantfile
---
+++ vagrant/manifests/site.pp
@@ -0,0 +1,95 @@
+node default {
+
+    Exec {
+        path => [
+            '/usr/local/sbin',
+            '/usr/local/bin',
+            '/usr/sbin',
+            '/usr/bin',
+            '/sbin',
+            '/bin'
+        ]
+    }
+
+    $home = "/home/vagrant"
+
+    # Configure apt
+
+    class { "apt":
+        always_apt_update => true,
+    }
+
+    include apt::update
+
+    Exec['apt_update'] -> Package <| |>
+
+    # Install some required packages
+
+    package {[
+            "ack-grep",
+            "binutils",
+            "cgdb",
+            "cmake",
+            "exuberant-ctags",
+            "g++",
+            "gcc",
+            "gdb",
+            "git",
+            "libxrandr-dev",
+            "libncurses5",
+            "libncurses5-dev",
+            "qemu",
+            "silversearcher-ag",
+            "tmux",
+            "autoconf",
+            "wget",
+            "python2.7",
+            "libjson-c-dev",
+            "libfuse-dev",
+        ]:
+        ensure => installed,
+    }
+
+    # Clone the skeleton files
+
+    Vcsrepo {
+        provider => git,
+        require  => Package[git],
+        user     => vagrant,
+        group    => vagrant,
+    }
+
+    file {
+        "$home/code/":
+            ensure => directory,
+            owner  => vagrant,
+            group  => vagrant;
+    }
+    ->
+    vcsrepo {
+        "$home/code/group":
+            ensure => present,
+            source => "git://github.com/Berkeley-CS162/group0.git",
+            remote => staff;
+        "$home/code/personal":
+            ensure => present,
+            source => "git://github.com/Berkeley-CS162/ta.git",
+            remote => staff;
+    }
+
+    # Set up some project support stuff
+
+    class { ["cs162::bochs", "cs162::shell"]:
+        home_directory => $home,
+        owner          => vagrant,
+        group          => vagrant,
+    }
+
+    include cs162::samba
+
+    file {
+        "/usr/bin/qemu":
+            ensure => "/usr/bin/qemu-system-x86_64";
+    }
+
+}
